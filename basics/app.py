913dfdc67404dc496cf454be221fd55a6bfda0f5
Muthuram <77574899+Muthuram-dev@users.noreply.github.com>
2025-04-10 20:15:46 +0530

    123

---
+++ basics/app.py
@@ -0,0 +1,12 @@
+print("hello world")
+
+# haha funny
+# number operations
+num1 = int(input("Enter the num1: "))
+num2 = int(input("Enter the num2: "))
+add = num1 + num2
+print(add)
+
+
+#expressions
+print("Hello" * 5)
