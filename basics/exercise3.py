14b1de5f1fc161b7cbd37679de7cc783c2a48870
Muthuram <77574899+Muthuram-dev@users.noreply.github.com>
2025-04-11 19:02:52 +0530

    Basics

---
+++ basics/exercise3.py
@@ -0,0 +1,3 @@
+pounds = int(input("Enter your weight in pounds: "))
+PtoKG = pounds * 0.453592
+print(f"You are {PtoKG} kilos!")
---
+++ basics/typeconversion.py
@@ -0,0 +1,5 @@
+birthyear = int(input("Enter your birth year: "))
+
+age = 2025 - birthyear
+print("You age is", age)
+print(f"You age is {age}")
