f5f08757aade9048d42d2e6982560001e285e2b8
Muthuram <77574899+Muthuram-dev@users.noreply.github.com>
2025-06-20 20:52:19 +0530

    test commit

--- basics/while-loop/exercise5.py
+++ basics/while-loop/exercise5.py
@@ -1,26 +1,44 @@
+# command = ""
+# started = False
+# while command != "quit":
+#     command = input(">").lower()
+#     if command == "help":
+#         print("""
+#                 start - to start the car
+#                 stop - to stop the car
+#                 quit - to exit""") 
+#     elif command == "start":
+#         if started:
+#             print("Car has been already started")
+#         else:
+#             print("Your car has been started")
+#             started = True
+#     elif command == "stop":
+#         if started == False:
+#             print("Your car has already been stopped")
+#         else:
+#             started = False
+#             print("Your car has been stopped")
+#     elif command == "quit":
+#         break
+#     else:
+#         print("Enter a valid command")
+# print("quitting...")
+
 command = ""
-started = False
+isStarted = False
 while command != "quit":
-    command = input(">").lower()
-    if command == "help":
+    if command == "start":
+        print("Car has been started!")
+        isStarted = True
+    elif command == "help":
         print("""
-                start - to start the car
-                stop - to stop the car
-                quit - to exit""") 
-    elif command == "start":
-        if started:
-            print("Car has been already started")
-        else:
-            print("Your car has been started")
-            started = True
+                 start - to start the car
+                 stop - to stop the car
+                 quit - to exit""") 
     elif command == "stop":
-        if started == False:
-            print("Your car has already been stopped")
-        else:
-            started = False
-            print("Your car has been stopped")
+        print("Car has been stopped")
+        isStarted = True
     elif command == "quit":
+        print("quitting the game...")
         break
-    else:
-        print("Enter a valid command")
-print("quitting...")
